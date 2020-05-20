4e69bd74bcf18bc130e765555e931490e7ab6af0
Chun-Wei Huang <huangc12@student.douglascollege.ca>
2020-05-19 23:11:39 -0700

    Updated myAppTester.java

---
+++ myJavaProject/src/myJavaProject/myAppTester.java
@@ -0,0 +1,30 @@
+package myJavaProject;
+
+import java.util.Scanner;
+
+public class myAppTester {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		
+		System.out.println("------ Software Engineering Lab ------");
+		System.out.println("------ Chun-Wei Huang / 300290304 ----");
+		
+		System.out.println("\n-- Addition");
+		
+		System.out.print("Please enter the firrst number:	");
+		a = input.nextInt();
+		
+		System.out.print("Please enter the second nunber:	");
+		b = input.nextInt();
+		addition(a, b);
+		
+	}
+	
+	public static void addition(int a, int b) {
+		int result = a + b;
+		System.out.println("The result of addition is " + result);
+	}
+
+}
